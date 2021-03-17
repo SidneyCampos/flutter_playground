3b1bbdba7c84c4a54145ca135e590d0981eee82b
SidneyCampos <sidneycampos22@gmail.com>
2021-03-17 17:33:04 -0300

    Projecto master piano training

--- master_piano_training/lib/main.dart
+++ master_piano_training/lib/main.dart
@@ -1,12 +1,66 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  // For lock orientation
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // FUNÇÕES
+
+  AudioCache player = AudioCache(prefix: '/assets/');
+
+  void playing() {
+    player.play('a.mp3');
+    setState(() {});
+  }
+
+  Card pianoKeys(Color keyColor, String keyNote, String keyName) {
+    return Card(
+      margin: EdgeInsets.all(0),
+      color: keyColor,
+      child: ListTile(
+        leading: Text(keyNote),
+        onTap: playing,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              pianoKeys(Colors.deepPurple, 'A', 'assets/a.wav'),
+              pianoKeys(Colors.blue, 'Bm', 'assets/bb.wav'),
+              pianoKeys(Colors.lightBlue, 'B', 'assets/b.wav'),
+              pianoKeys(Colors.cyan, 'C', 'assets/c.wav'),
+              pianoKeys(Colors.teal, 'C#', 'assets/c#.wav'),
+              pianoKeys(Colors.green, 'D', 'assets/d.wav'),
+              pianoKeys(Colors.lightGreen, 'Em', 'assets/eb.wav'),
+              pianoKeys(Colors.lime, 'F', 'assets/f.wav'),
+              pianoKeys(Colors.yellow, 'F#', 'assets/f#.wav'),
+              pianoKeys(Colors.amber, 'G', 'assets/g.wav'),
+              pianoKeys(Colors.orange, 'G#', 'assets/g#.wav'),
+              pianoKeys(Colors.deepPurple, 'A', 'assets/a.wav'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- master_piano_training/test/widget_test.dart
+++ master_piano_training/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:master_piano_training/main.dart';
+import 'file:///C:/Users/sidne/AndroidStudioProjects/master_piano_training/lib/views/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
